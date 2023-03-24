dce9984a4c59ff5de3222cf0314c6b2245fb449f
Benta <bentaakitwi500@gmail.com>
2023-03-24 11:37:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("Dom hasloaded")
+let parTag =document.getElementById("text")
+parTag.textContent = "This is really cool!"
+})
+console.log("Load Dom before event listener")
